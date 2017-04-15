a41772a5c47146f60e1321741f01e481c13c25fe
an-yun <zsy296@gmail.com>
2017-04-15 16:02:04 +0800

    adjust some settings and add testcase

--- Regex/Regex/test/NFATest.cpp
+++ Regex/Regex/test/NFATest.cpp
@@ -758,6 +758,19 @@ namespace anyun_regex
 		test_group();
 
 		//here need more testcases
+
+		set_pattern("a?(ab)+?""(ab)+");
+		add_testcase("aab");
+		add_testcase("aaab");
+		add_testcase("aabab");
+		add_testcase("aaaaab");
+		add_testcase("acab");
+		add_testcase("babaaaab");
+		add_testcase("bbabaabab");
+		add_testcase("bbbbbbbbbbbbbbbbbbbba");
+		add_testcase("abababababababababab");
+		add_testcase("123ab132aab2ababd12f9kababab");
+		test_group();
 	}
 
 	void NFATest::test_lazy_match_repeat_count()
