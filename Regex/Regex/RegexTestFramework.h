167033234a83f16225842d41683c1cd5220378e0
an-yun <zsy296@gmail.com>
2017-04-26 11:22:09 +0800

    add new test api

--- Regex/Regex/RegexTestFramework.h
+++ Regex/Regex/RegexTestFramework.h
@@ -28,12 +28,12 @@ namespace regex_test_data = boost::unit_test::data;
 typedef std::pair<const char*, const char*> pre_process_test_case_t;
 BOOST_TEST_DONT_PRINT_LOG_VALUE(pre_process_test_case_t)
 
-//perform one test for pre process pattern
-#define TEST_PRE_PROCESS_PATTERN(pattern,after_pre_process_pattern)													\
+//perform one normal test for pre process pattern
+#define TEST_NORMAL_PRE_PROCESS_PATTERN(pattern,after_pre_process_pattern)											\
 	do{																												\
 		::anyun_regex::DirectedGraph test_diagraph;																	\
 		::std::string pre_process_result = test_diagraph.pre_process_pattern(pattern);								\
-		::std::cout<<"test DirectedGraph.pre_process_pattern for '"<<pattern<<"'"<<std::endl;						\
+		::std::cout<<"test normal DirectedGraph.pre_process_pattern for '"<<pattern<<"'"<<std::endl;				\
 		::anyun_regex::RegexParseCode parse_result_code = test_diagraph.get_parse_result_code();					\
 		BOOST_TEST(pre_process_result == (after_pre_process_pattern),												\
 				   "the pre process test result for '" << pattern <<	"' is '"<<pre_process_result				\
@@ -44,13 +44,33 @@ BOOST_TEST_DONT_PRINT_LOG_VALUE(pre_process_test_case_t)
 				   <<"' (expected:'REGEX_PARSE_OK')");																\
 	}while(false)
 
+//perform one error test for pre process pattern
+#define TEST_ERROR_PRE_PROCESS_PATTERN(pattern,excepted_parase_error_code)											\
+	do{																												\
+		::anyun_regex::DirectedGraph test_diagraph;																	\
+		::std::string pre_process_result = test_diagraph.pre_process_pattern(pattern);								\
+		::std::cout<<"test error DirectedGraph.pre_process_pattern for '"<<pattern<<"'"<<std::endl;					\
+		::anyun_regex::RegexParseCode parse_result_code = test_diagraph.get_parse_result_code();					\
+		BOOST_TEST(pre_process_result == "",																		\
+				   "the pre process test result for '" << pattern <<	"' is '"<<pre_process_result				\
+				   <<"' (expected:'')");																			\
+		BOOST_TEST(parse_result_code == excepted_parase_error_code,													\
+				  "the pre process test result code for '" << pattern <<	"' is'"									\
+				  <<::anyun_regex::PARSE_RESULT_MESSAGE[parse_result_code]											\
+				   <<"' (expected:'"<<::anyun_regex::PARSE_RESULT_MESSAGE[excepted_parase_error_code]<<"')");																\
+	}while(false)
+
 //add one test case for pre process pattern
-#define ADD_ONE_TEST_CASE_FOR_PRE_PROCESS_PATTERN(test_case_name,pattern,after_pre_process_pattern)					\
-	ADD_ONE_TEST_CASE(test_case_name) {TEST_PRE_PROCESS_PATTERN(pattern,after_pre_process_pattern);}
+#define ADD_ONE_NORMAL_TEST_CASE_FOR_PRE_PROCESS_PATTERN(test_case_name,pattern,after_pre_process_pattern)			\
+	ADD_ONE_TEST_CASE(test_case_name) {TEST_NORMAL_PRE_PROCESS_PATTERN(pattern,after_pre_process_pattern);}
 
-
+//add normal test case for pre process pattern
 #define ADD_NORMAL_TESTCASE_FOR_PRE_PROCESS(test_case_name,testcases)												\
-	ADD_DATA_TEST_CASE(test_case_name,testcases,one_testcase)	{TEST_PRE_PROCESS_PATTERN(one_testcase.first,one_testcase.second);}													
+	ADD_DATA_TEST_CASE(test_case_name,testcases,one_testcase)	{TEST_NORMAL_PRE_PROCESS_PATTERN(one_testcase.first,one_testcase.second);}	
+
+//add error test case for pre process pattern
+#define ADD_ERROR_TESTCASE_FOR_PRE_PROCESS(test_case_name,testcases)												\
+	ADD_DATA_TEST_CASE(test_case_name,testcases,one_testcase)	{TEST_ERROR_PRE_PROCESS_PATTERN(one_testcase.first,one_testcase.second);}	
 
 //test suit macro
 #define BEGING_TEST_SUIT(test_suit_name) BOOST_AUTO_TEST_SUITE(test_suit_name)
--- Regex/Regex/test/test_main.cpp
+++ Regex/Regex/test/test_main.cpp
@@ -11,7 +11,7 @@
  *add all test moudules that shoul test 
  */
 #define REGEX_TEST_MOUDLE regex test
-#include "PreProcessTest.h"
+#include "PreProcessNormalTest.h"
 #include "PreProcessTest1.h"
 
 #else
